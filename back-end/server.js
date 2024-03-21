a66211744e5ba8dd2304e4e88d731f3c015851ba
Kapil Dev Pandey <kapilpandeyweb@gmail.com>
2024-03-21 11:36:07 +0530

    inital commit
    mern application

---
+++ back-end/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const connectDB = require("./config/db_config");
+const router = require("./routes/bagRoutes");
+const cookieParser = require("cookie-parser");
+const errorHandler = require("./middleware/errorhandler");
+const cors = require("cors");
+const path = require("path");
+
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+connectDB();
+if(process.env.NODE_ENV === "production"){
+  app.use(cors())
+}else{
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      methods: ["get", "post"],
+      credentials: true,
+    })
+  );
+}
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.json());
+
+app.use("/api/products", router);
+app.use(errorHandler);
+app.use(express.static(path.join(__dirname,"../front-end/dist")));
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname ,'../front-end/dist/index.html' ))
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running at : ${PORT}`);
+});
